647ca51df0317b611d67d7937d170a041f196b68
Arman <arman458792@gmail.com>
2024-09-10 17:53:22 +0500

    done

---
+++ src/ContactForm.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+
+export default function ContactForm() {
+  return (
+    <div id="consultation-form" className="consultation-form">
+        <h2 className="form-title">Запись на консультацию</h2>
+        <div className="form-content">
+          <div className="row ">
+            <div className="col-sm-6">
+              <div className="consultation-info">
+                <h3>
+                  Оставьте заявку и получите бесплатную консультацию от врача.
+                  Индивидуальный план лечения в подарок
+                </h3>
+              </div>
+            </div>
+            <div className="col-sm-6">
+              <form className="form-fields">
+                <input type="text" placeholder="Имя" required />
+                <input type="text" placeholder="Телефон" required />
+                <button type="submit" className="submit-btn">
+                  Записаться
+                </button>
+                <label className="checkbox-container">
+                  <input type="checkbox" required />
+                  <span>Я согласен на обработку своих персональных данных</span>
+                </label>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+  )
+}
